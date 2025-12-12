10e1476fbd837f187e825600029bc08295ba0cb7
irfan-official <irfanofficial.work@gmail.com>
2025-12-12 20:12:42 +0600

    removed share-review

--- app/share-reviews/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShareReviews from "@/components/ShareReviews";
-
-export default function Page() {
-  return (
-    <section className="w-full mx-auto h-[100vh]  flex items-center justify-center">
-      <ShareReviews />
-    </section>
-  );
-}
